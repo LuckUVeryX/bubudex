af4d87ee5188d971d45bcafcdef6039861683c6d
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2021-10-19 18:09:32 +0800

    refactored search provider to reduce unnecessary rebuilds

--- lib/view_models/home_provider.dart
+++ lib/view_models/home_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 
 import '../models/models.dart';
 import '../repository/repository.dart';
@@ -7,25 +8,22 @@ enum HomePageStatus { init, done, error }
 
 class HomeProvider extends ChangeNotifier {
   HomeProvider(this._repository);
-
   final IPokeRepository _repository;
 
   HomePageStatus _status = HomePageStatus.init;
   HomePageStatus get status => _status;
 
-  late final List<Pokemon> _pokemons;
-
+  List<Pokemon> _pokemons = [];
   List<Pokemon> get pokemons => _pokemons;
 
-  Future<void> getPokemons() async {
+  void getPokemons() async {
     try {
       _pokemons = await _repository.getPokemons();
       _status = HomePageStatus.done;
     } catch (e) {
       _status = HomePageStatus.error;
       debugPrint(e.toString());
-    } finally {
-      notifyListeners();
     }
+    notifyListeners();
   }
 }
--- lib/view_models/view_models.dart
+++ lib/view_models/view_models.dart
@@ -1,1 +1,2 @@
 export 'home_provider.dart';
+export 'search_provider.dart';
