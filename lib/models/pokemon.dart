d880fa8f4d23f3f81ada121fb1fe58c3d067615b
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2021-10-19 21:57:13 +0800

    added typedefenses, height and weight to pokemon model

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -18,12 +18,20 @@ class Pokemon {
   String image;
 
   @HiveField(3)
+  double height;
+
+  @HiveField(4)
+  double weight;
+
+  @HiveField(5)
   List<String> types;
 
   Pokemon({
     required this.id,
     required this.name,
     required this.image,
+    required this.height,
+    required this.weight,
     required this.types,
   });
 
@@ -31,6 +39,96 @@ class Pokemon {
       _$PokemonFromJson(json);
 
   Map<String, dynamic> toJson() => _$PokemonToJson(this);
+
+  @override
+  String toString() {
+    return toJson().toString();
+  }
+}
+
+@JsonSerializable()
+@HiveType(typeId: HiveTypeId.typeDefenses)
+class TypeDefences {
+  @HiveField(6)
+  double normal;
+
+  @HiveField(7)
+  double fire;
+
+  @HiveField(8)
+  double water;
+
+  @HiveField(9)
+  double electric;
+
+  @HiveField(10)
+  double grass;
+
+  @HiveField(11)
+  double ice;
+
+  @HiveField(12)
+  double fighting;
+
+  @HiveField(13)
+  double poison;
+
+  @HiveField(14)
+  double ground;
+
+  @HiveField(15)
+  double flying;
+
+  @HiveField(16)
+  double psychic;
+
+  @HiveField(17)
+  double bug;
+
+  @HiveField(18)
+  double rock;
+
+  @HiveField(19)
+  double ghost;
+
+  @HiveField(20)
+  double dragon;
+
+  @JsonKey(name: 'darl')
+  @HiveField(21)
+  double dark;
+
+  @HiveField(22)
+  double steel;
+
+  @HiveField(23)
+  double fairy;
+
+  TypeDefences({
+    required this.normal,
+    required this.fire,
+    required this.water,
+    required this.electric,
+    required this.grass,
+    required this.ice,
+    required this.fighting,
+    required this.poison,
+    required this.ground,
+    required this.flying,
+    required this.psychic,
+    required this.bug,
+    required this.rock,
+    required this.ghost,
+    required this.dragon,
+    required this.dark,
+    required this.steel,
+    required this.fairy,
+  });
+
+  factory TypeDefences.fromJson(Map<String, dynamic> json) =>
+      _$TypeDefencesFromJson(json);
+
+  Map<String, dynamic> toJson() => _$TypeDefencesToJson(this);
 
   @override
   String toString() {
--- lib/models/pokemon.g.dart
+++ lib/models/pokemon.g.dart
@@ -20,14 +20,16 @@ class PokemonAdapter extends TypeAdapter<Pokemon> {
       id: fields[0] as int,
       name: fields[1] as String,
       image: fields[2] as String,
-      types: (fields[3] as List).cast<String>(),
+      height: fields[3] as double,
+      weight: fields[4] as double,
+      types: (fields[5] as List).cast<String>(),
     );
   }
 
   @override
   void write(BinaryWriter writer, Pokemon obj) {
     writer
-      ..writeByte(4)
+      ..writeByte(6)
       ..writeByte(0)
       ..write(obj.id)
       ..writeByte(1)
@@ -35,6 +37,10 @@ class PokemonAdapter extends TypeAdapter<Pokemon> {
       ..writeByte(2)
       ..write(obj.image)
       ..writeByte(3)
+      ..write(obj.height)
+      ..writeByte(4)
+      ..write(obj.weight)
+      ..writeByte(5)
       ..write(obj.types);
   }
 
@@ -49,6 +55,91 @@ class PokemonAdapter extends TypeAdapter<Pokemon> {
           typeId == other.typeId;
 }
 
+class TypeDefencesAdapter extends TypeAdapter<TypeDefences> {
+  @override
+  final int typeId = 1;
+
+  @override
+  TypeDefences read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TypeDefences(
+      normal: fields[6] as double,
+      fire: fields[7] as double,
+      water: fields[8] as double,
+      electric: fields[9] as double,
+      grass: fields[10] as double,
+      ice: fields[11] as double,
+      fighting: fields[12] as double,
+      poison: fields[13] as double,
+      ground: fields[14] as double,
+      flying: fields[15] as double,
+      psychic: fields[16] as double,
+      bug: fields[17] as double,
+      rock: fields[18] as double,
+      ghost: fields[19] as double,
+      dragon: fields[20] as double,
+      dark: fields[21] as double,
+      steel: fields[22] as double,
+      fairy: fields[23] as double,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TypeDefences obj) {
+    writer
+      ..writeByte(18)
+      ..writeByte(6)
+      ..write(obj.normal)
+      ..writeByte(7)
+      ..write(obj.fire)
+      ..writeByte(8)
+      ..write(obj.water)
+      ..writeByte(9)
+      ..write(obj.electric)
+      ..writeByte(10)
+      ..write(obj.grass)
+      ..writeByte(11)
+      ..write(obj.ice)
+      ..writeByte(12)
+      ..write(obj.fighting)
+      ..writeByte(13)
+      ..write(obj.poison)
+      ..writeByte(14)
+      ..write(obj.ground)
+      ..writeByte(15)
+      ..write(obj.flying)
+      ..writeByte(16)
+      ..write(obj.psychic)
+      ..writeByte(17)
+      ..write(obj.bug)
+      ..writeByte(18)
+      ..write(obj.rock)
+      ..writeByte(19)
+      ..write(obj.ghost)
+      ..writeByte(20)
+      ..write(obj.dragon)
+      ..writeByte(21)
+      ..write(obj.dark)
+      ..writeByte(22)
+      ..write(obj.steel)
+      ..writeByte(23)
+      ..write(obj.fairy);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TypeDefencesAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
@@ -57,6 +148,8 @@ Pokemon _$PokemonFromJson(Map<String, dynamic> json) => Pokemon(
       id: json['id'] as int,
       name: json['name'] as String,
       image: json['image'] as String,
+      height: (json['height'] as num).toDouble(),
+      weight: (json['weight'] as num).toDouble(),
       types: (json['types'] as List<dynamic>).map((e) => e as String).toList(),
     );
 
@@ -64,5 +157,50 @@ Map<String, dynamic> _$PokemonToJson(Pokemon instance) => <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
       'image': instance.image,
+      'height': instance.height,
+      'weight': instance.weight,
       'types': instance.types,
+    };
+
+TypeDefences _$TypeDefencesFromJson(Map<String, dynamic> json) => TypeDefences(
+      normal: (json['normal'] as num).toDouble(),
+      fire: (json['fire'] as num).toDouble(),
+      water: (json['water'] as num).toDouble(),
+      electric: (json['electric'] as num).toDouble(),
+      grass: (json['grass'] as num).toDouble(),
+      ice: (json['ice'] as num).toDouble(),
+      fighting: (json['fighting'] as num).toDouble(),
+      poison: (json['poison'] as num).toDouble(),
+      ground: (json['ground'] as num).toDouble(),
+      flying: (json['flying'] as num).toDouble(),
+      psychic: (json['psychic'] as num).toDouble(),
+      bug: (json['bug'] as num).toDouble(),
+      rock: (json['rock'] as num).toDouble(),
+      ghost: (json['ghost'] as num).toDouble(),
+      dragon: (json['dragon'] as num).toDouble(),
+      dark: (json['darl'] as num).toDouble(),
+      steel: (json['steel'] as num).toDouble(),
+      fairy: (json['fairy'] as num).toDouble(),
+    );
+
+Map<String, dynamic> _$TypeDefencesToJson(TypeDefences instance) =>
+    <String, dynamic>{
+      'normal': instance.normal,
+      'fire': instance.fire,
+      'water': instance.water,
+      'electric': instance.electric,
+      'grass': instance.grass,
+      'ice': instance.ice,
+      'fighting': instance.fighting,
+      'poison': instance.poison,
+      'ground': instance.ground,
+      'flying': instance.flying,
+      'psychic': instance.psychic,
+      'bug': instance.bug,
+      'rock': instance.rock,
+      'ghost': instance.ghost,
+      'dragon': instance.dragon,
+      'darl': instance.dark,
+      'steel': instance.steel,
+      'fairy': instance.fairy,
     };
