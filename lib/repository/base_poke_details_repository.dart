728697cc73045d74e05c0612ba34c3b899abf59c
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2021-10-20 21:09:59 +0800

    created base repository

---
+++ lib/repository/base_poke_details_repository.dart
@@ -0,0 +1,3 @@
+abstract class IPokeDetailsRepository {
+  Future getPokeDetails();
+}
--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -1,2 +1,3 @@
+export 'base_poke_details_repository.dart';
 export 'base_poke_repository.dart';
 export 'poke_repository.dart';
